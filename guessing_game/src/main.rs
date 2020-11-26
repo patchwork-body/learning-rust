4f4d92d13819601add5a5200c9a3700b8bf8219b
gugfug1 <gugfug1@gmail.com>
2020-11-26 06:38:06 +0000

    output user prompt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to guessing game!");
+    println!("Guess your number:");
+
+    let mut user_guess = String::new();
+
+    io::stdin()
+        .read_line(&mut user_guess)
+        .expect("Failed to read user prompt");
+
+    println!("Your guess is {}", user_guess);
 }
